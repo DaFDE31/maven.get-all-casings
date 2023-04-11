c39ea24d63093b83930daa4bc9d2a9bfbe57c90f
danielson <dadjocy21@students.desu.edu>
2023-04-11 10:25:49 -0400

    progress

--- src/main/java/com/github/curriculeon/PowerSet.java
+++ src/main/java/com/github/curriculeon/PowerSet.java
@@ -11,8 +11,10 @@ public class PowerSet<TypeOfSet> {
     TypeOfSet[] og_set;
 
     Set<Set<TypeOfSet>> power_set = new HashSet<>();
+    Set<TypeOfSet [] > power_set2 = new HashSet<>();
 
     public PowerSet(TypeOfSet[] originalSet) {
+        /*
         //set = originalSet;
         Set<TypeOfSet> set2 = new HashSet<>();
         for (int one = 0; one <originalSet.length; one++){
@@ -24,6 +26,19 @@ public class PowerSet<TypeOfSet> {
                 partial_set.add(originalSet[others]);
             }
             power_set.add(partial_set);
+        }
+
+         *///Attempt 1
+        int size = 0;
+        while (size <= originalSet.length){
+            //Go through every letter
+            TypeOfSet [] subset = (TypeOfSet[]) new Object[size];
+            for(int i = 0; i<subset.length; i++){
+                subset[i] = originalSet[i];
+            }
+            power_set2.add(subset);
+            //if ()
+            size++;
         }
     }
 
